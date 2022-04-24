affdb63d4fd72828e7ee52b84d6a6ed9849abfe9
lcminhbu <lminhbuu@gmail.com>
2022-04-24 12:25:28 +0700

    user-customer relationship
    add user-customer one-to-one relationship

--- accounts/views.py
+++ accounts/views.py
@@ -27,6 +27,7 @@ def registerPage(request):
             username = form.cleaned_data.get('username')
             group = Group.objects.get(name='customer')
             user.groups.add(group)
+            Customer.objects.create(user=user)
             messages.success(request, 'Account was created for ' + username)
             return redirect('login')
     context = {'form': form}
@@ -74,8 +75,15 @@ def home(request):
     return render(request, 'accounts/dashboard.html', context)
 
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
+    orders = request.user.customer.order_set.all()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    print('orders: ', orders)
+    context = {'orders': orders, 'total_orders': total_orders, 'delivered': delivered, 'pending': pending}
     return render(request, 'accounts/user.html', context)
 
 
